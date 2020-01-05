c318e7b36bebd9fefe4ae0c38da32f71734b2691
Anthony Good <good.anthony@gmail.com>
2020-01-05 14:00:06 +0000

    tweak Render.bodies.override.js

--- src/matter/Render.bodies.override.js
+++ src/matter/Render.bodies.override.js
@@ -2,12 +2,13 @@ import { Render } from 'matter-js'
 
 const defaultRenderBodies = Render.bodies
 
+const renderWithBodyPlugin = context => body =>
+  body.plugin.render && body.plugin.render(body, context)
+
 // Slightly inefficient since it iterates bodies again.
 const renderBodiesWithPlugin = (_render, bodies, context) => {
   defaultRenderBodies(_render, bodies, context)
-  bodies.forEach(bod =>
-    bod.plugin.render && bod.plugin.render(bod, context)
-  )
+  bodies.forEach(renderWithBodyPlugin(context))
 }
 
 // Override Matter.Render.bodies
